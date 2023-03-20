e478f1e5e912f15a5da68712f6c5c2ef370134d6
SerovaK4 <ksusha1354@gmail.com>
2023-03-21 01:22:30 +0300

    exercise 5

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -4,9 +4,9 @@ from utils import dicts
 class TestDict(unittest.TestCase):
 
     def test_get(self):
-        self.assertEqual(dicts.get_val({"baba": "you"}, "baba", "git"), "you")
-        self.assertEqual(dicts.get_val({"baba": "you"}, "hehe", "error"), "error")
-        self.assertEqual(dicts.get_val({"baba": "you", "life": "pain"}, "life", "git"), "pain")
+        self.assertEqual(dicts.get_val({"dog": "woof"}, "dog", "git"), "woof")
+        self.assertEqual(dicts.get_val({"dog": "woof"}, "baa", "error"), "error")
+        self.assertEqual(dicts.get_val({"dog": "woof", "frog": "ribbit"}, "frog", "git"), "ribbit")
 
 
 
